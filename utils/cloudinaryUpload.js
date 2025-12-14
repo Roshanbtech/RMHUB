ad68f7c1a5837a02614e9e1e575cf83b896736ce
Roshan Reji <84395399+Roshanbtech@users.noreply.github.com>
2025-12-14 23:53:13 +0530

    new util for cloudinaryUpload

---
+++ utils/cloudinaryUpload.js
@@ -0,0 +1,15 @@
+const cloudinary = require("../config/cloudinary");
+const streamifier = require("streamifier");
+
+const uploadBufferToCloudinary = (buffer, options = {}) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(options, (error, result) => {
+      if (error) return reject(error);
+      resolve(result);
+    });
+
+    streamifier.createReadStream(buffer).pipe(stream);
+  });
+};
+
+module.exports = { uploadBufferToCloudinary };
